0e81c101fb710f25e3be8c41d2f154cf49b61016
DAMIR <d.setkaliev@yandex.ru>
2025-06-29 11:32:18 +0600

    feat: add string extension method IsPalindrome

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            input = input.ToLower();
+            string str_new = string.Empty;
+
+            foreach (char chr in input)
+            {
+                if (!char.IsPunctuation(chr) && !char.IsWhiteSpace(chr))
+                    str_new += chr;
+            }
+
+            string str_compare = new string(str_new.Reverse().ToArray());
+            return str_compare == str_new;
+        }
+    }
+}
