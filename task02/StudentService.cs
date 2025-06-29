75ddd602208cea0965f2f218bdb8a4d858dd8885
DAMIR <d.setkaliev@yandex.ru>
2025-06-29 11:46:56 +0600

    feat: add StudentService class

---
+++ task02/StudentService.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace task02;
+
+public class Student
+{
+    public required string Name { get; set; }
+    public required string Faculty { get; set; }
+    public required List<int> Grades { get; set; }
+}
+
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+        => _students.Where(s => s.Faculty == faculty);
+
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)
+        => _students.Where(s => s.Grades.Average() > minAverageGrade);
+
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => _students.OrderBy(s => s.Name);
+
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => _students.ToLookup(s => s.Faculty, s => s);
+
+    public string GetFacultyWithHighestAverageGrade()
+        => _students.GroupBy(s => s.Faculty).OrderBy(g => g.SelectMany(s => s.Grades).Average()).Last().Key;
+}
---
+++ task02tests/StudentServiceTests.cs
@@ -0,0 +1,61 @@
+﻿﻿using Xunit;
+using task02;
+
+namespace task02tests;
+
+public class StudentServiceTests
+{
+    private List<Student> _testStudents;
+    private StudentService _service;
+
+    public StudentServiceTests()
+    {
+        _testStudents = new List<Student>
+        {
+            new() { Name = "Иван", Faculty = "ФИТ", Grades = new List<int> { 5, 4, 5 } },
+            new() { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> { 3, 4, 3 } },
+            new() { Name = "Петр", Faculty = "Экономика", Grades = new List<int> { 5, 5, 5 } }
+        };
+        _service = new StudentService(_testStudents);
+    }
+
+    [Fact]
+    public void GetStudentsByFaculty_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsByFaculty("ФИТ").ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
+    }
+
+    [Fact]
+    public void GetStudentsWithMinAverageGrade_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsWithMinAverageGrade(4).ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Grades.Average() > 4));
+    }
+
+    [Fact]
+    public void GetStudentsOrderedByName_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsOrderedByName().ToList();
+        Assert.Equal(3, result.Count);
+        string[] expected = new string[3] {"Анна", "Иван", "Петр"};
+        Assert.Equal(expected, result.Select(s => s.Name));
+    }
+
+    [Fact]
+    public void GroupStudentsByFaculty_ReturnsCorrectStudents()
+    {
+        var result = _service.GroupStudentsByFaculty().ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result[0].All(s => s.Faculty == "ФИТ") && result[1].All(s => s.Faculty == "Экономика"));
+    }
+    
+    [Fact]
+    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
+    {
+        var result = _service.GetFacultyWithHighestAverageGrade();
+        Assert.Equal("Экономика", result);
+    }
+}
