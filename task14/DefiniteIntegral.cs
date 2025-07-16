1b99ad46d596e22ee5945589c510d9d1f74a6695
DAMIR <d.setkaliev@yandex.ru>
2025-07-17 00:23:23 +0600

    feat: add multithread definite integral calculation

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading;
+
+namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        if (threadsNumber < 1)
+            throw new ArgumentException("Количество потоков должно быть по крайней мере равно 1", nameof(threadsNumber));
+        if (step <= 0)
+            throw new ArgumentException("Шаг интегрирования должен быть положительным", nameof(step));
+        if (b <= a)
+            throw new ArgumentException("Верхний предел интегрирования должен быть больше нижнего");
+        
+        double result = 0;
+
+        Barrier barrier = new Barrier(threadsNumber + 1);
+
+        double segmentLength = (b - a) / threadsNumber;
+        
+        Thread[] threads = new Thread[threadsNumber];
+        
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            double localStart = a + i * segmentLength;
+            double localEnd;
+            if (i == threadsNumber - 1)
+                localEnd = b;
+            else
+                localEnd = localStart + segmentLength;
+            threads[i] = new Thread(() =>
+            {
+                double partialSum = 0;
+                double x = localStart;
+                while (x < localEnd)
+                {
+                    double xNext = Math.Min(x + step, localEnd);
+                    partialSum += (function(x) + function(xNext)) * (xNext - x) / 2;
+                    x = xNext;
+                }
+
+                double initial, computed;
+                do
+                {
+                    initial = result;
+                    computed = initial + partialSum;
+                }
+                while (Interlocked.CompareExchange(
+                    ref result,
+                    computed,
+                    initial) != initial);
+
+                barrier.SignalAndWait();
+            });
+            threads[i].Start();
+        }
+        barrier.SignalAndWait();
+        return result;
+    }
+}
---
+++ task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,27 @@
+﻿using task14;
+
+namespace task14tests;
+
+public class DefiniteIntegralTests
+{
+    private static readonly Func<double, double> X = x => x;
+    private static readonly Func<double, double> SIN = x => Math.Sin(x);
+
+    [Fact]
+    public void IntegralOfIdentityFromMinus1To1_IsZero()
+    {
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void IntegralOfSinFromMinus1To1_IsZero()
+    {
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void IntegralOfIdentityFrom0To5_IsTen()
+    {
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
